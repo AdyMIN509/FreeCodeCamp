e57341b1a96236419045fe8f84e3aa59620291db
Ady_MIN <mbolanantenainachristian@gmail.com>
2026-08-11 20:48:13 +0300

    Javascript Start

---
+++ ResponsiveWebDesignCertification/JavaScript/BuildASentenceMaker/script.js
@@ -0,0 +1,21 @@
+let adjective = "sleepy";
+let noun = "penguin";
+let verb = "dancing";
+let place = "Antananarivo";
+let adjective2 = "gigantic";
+let noun2 = "keyboard";
+
+let firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+ noun2 +". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+
+console.log("First story: " + firstStory);
+
+adjective = "grumpy";
+noun = "dragon";
+verb = "juggling";
+place = "the moon";
+adjective2 = "tiny";
+noun2 = "spaceship";
+
+let secondStory
+secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+ noun2 +". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+console.log("Second story: " + secondStory);
